6b759282dcfa00522174dc9173bac36267a71bd6
admin <admin@admindeMacBook-Pro.local>
2022-10-07 13:47:41 +0800

    chapter1-3 实模式下的MBR加载器

---
+++ code/chapter1-3/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+if [ ! -d "./out" ]; then
+      mkdir out
+fi
+nasm -I include/ -o ./out/mbr.bin mbr.S && dd if=./out/mbr.bin of=./hd3M.img bs=512 count=1  conv=notrunc
+nasm -I include/ -o ./out/loader.bin loader.S && dd if=./out/loader.bin of=./hd3M.img bs=512 count=1 seek=2 conv=notrunc
+/usr/local/bin/bochs -f bochsrc
