662a18caa24ef44f33bde0d2bbf28784bd6cb13f
admin <admin@admindeMacBook-Pro.local>
2022-10-18 21:21:03 +0800

    打印字符串

--- code/chapter6/b/kernel/main.c
+++ code/chapter6/b/kernel/main.c
@@ -1,24 +1,6 @@
 #include "print.h"
 void main(void) {
-    put_char('k');
-    put_char('e');
-    put_char('r');
-    put_char('n');
-    put_char('e');
-    put_char('l');
-    put_char('\n');
-    put_char('p');
-    put_char('w');
-    put_char('e');
-    put_char('n');
-    put_char('g');
-    put_char('u');
-    put_char('o');
-    put_char('\n');
-    put_char('1');
-    put_char('2');
-    put_char('\b');
-    put_char('3');
-    
+    put_str("kernel\n");
+    put_str("hello world");
     while(1);
 }
--- code/chapter6/b/lib/kernel/print.h
+++ code/chapter6/b/lib/kernel/print.h
@@ -2,4 +2,5 @@
 #define __LIB_KERNEL_PRINT_H
 #include "std_int.h"
 void put_char(uint8_t char_asci);
+void put_str(char* message);
 #endif
