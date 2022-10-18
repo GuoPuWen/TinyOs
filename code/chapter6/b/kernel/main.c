e455f4432d34802450ab61e67b8f92c355849a6f
admin <admin@admindeMacBook-Pro.local>
2022-10-18 20:51:19 +0800

    fix bug

--- code/chapter6/b/kernel/main.c
+++ code/chapter6/b/kernel/main.c
@@ -7,24 +7,18 @@ void main(void) {
     put_char('e');
     put_char('l');
     put_char('\n');
-    put_char('y');
-    put_char('o');
-    put_char('u');
-    put_char('y');
-    put_char('i');
-    put_char('f');
+    put_char('p');
+    put_char('w');
     put_char('e');
     put_char('n');
     put_char('g');
+    put_char('u');
+    put_char('o');
     put_char('\n');
     put_char('1');
     put_char('2');
     put_char('\b');
     put_char('3');
-    int i=100;
-    while(i--)
-    {
-        put_char('y');
-    };
+    
     while(1);
 }
