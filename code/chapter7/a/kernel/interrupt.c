d17794f041b77ec7206e4e3f6e7a183d0f1050f8
admin <admin@admindeMacBook-Pro.local>
2022-10-30 15:40:23 +0800

    改进中断处理程序

--- code/chapter7/a/kernel/interrupt.c
+++ code/chapter7/a/kernel/interrupt.c
@@ -8,7 +8,6 @@
 #define PIC_M_DATA 0x21	       // 主片的数据端口是0x21
 #define PIC_S_CTRL 0xa0	       // 从片的控制端口是0xa0
 #define PIC_S_DATA 0xa1	       // 从片的数据端口是0xa1
-
 //中断描述符
 struct gate_desc{
     uint16_t func_offset_low_word;
@@ -19,11 +18,56 @@ struct gate_desc{
 };
 
 
+char* intr_name[IDT_DESC_CNT];          //保存异常的名字
+
+intr_handler idt_table[IDT_DESC_CNT];       //中断处理程序数组
+
+
 static void make_idt_desc(struct gate_desc* p_gdesc, uint8_t attr, intr_handler function);
 
 static struct gate_desc idt[IDT_DESC_CNT];      //中断描述符数组
 
 extern intr_handler intr_entry_table[IDT_DESC_CNT];     //中断程序函数入口数组
+
+//通用的中断处理函数
+static void general_intr_handler(uint8_t vec_nr) {
+    // IRQ7和IRQ15会产生伪中断(spurious interrupt)，无需处理
+    if(vec_nr == 0x27 || vec_nr == 0x2f) {
+        return;         
+    }
+    put_str("int vector: 0x");
+    put_int(vec_nr);
+    put_char('\n');
+}
+
+//中断注册函数 往中断程序数组里填充数据
+static void exception_init(void) {
+    int i = 0;
+    for(;i < IDT_DESC_CNT;i++) {
+        idt_table[i] = general_intr_handler;
+        intr_name[i] = "unknown";                   // 先统一赋值为 unknown     
+    }
+   intr_name[0] = "#DE Divide Error";
+   intr_name[1] = "#DB Debug Exception";
+   intr_name[2] = "NMI Interrupt";
+   intr_name[3] = "#BP Breakpoint Exception";
+   intr_name[4] = "#OF Overflow Exception";
+   intr_name[5] = "#BR BOUND Range Exceeded Exception";
+   intr_name[6] = "#UD Invalid Opcode Exception";
+   intr_name[7] = "#NM Device Not Available Exception";
+   intr_name[8] = "#DF Double Fault Exception";
+   intr_name[9] = "Coprocessor Segment Overrun";
+   intr_name[10] = "#TS Invalid TSS Exception";
+   intr_name[11] = "#NP Segment Not Present";
+   intr_name[12] = "#SS Stack Fault Exception";
+   intr_name[13] = "#GP General Protection Exception";
+   intr_name[14] = "#PF Page-Fault Exception";
+   // intr_name[15] 第15项是intel保留项，未使用
+   intr_name[16] = "#MF x87 FPU Floating-Point Error";
+   intr_name[17] = "#AC Alignment Check Exception";
+   intr_name[18] = "#MC Machine-Check Exception";
+   intr_name[19] = "#XF SIMD Floating-Point Exception";
+}
 
 // 填充中断描述符
 static void make_idt_desc(struct gate_desc* p_gdesc, uint8_t attr, intr_handler function) {
@@ -71,6 +115,8 @@ void idt_init() {
     put_str("idt_init start\n");
 
     idt_desc_init();
+
+    exception_init();
 
     pic_init();
 
